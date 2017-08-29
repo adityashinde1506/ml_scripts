15b0c727cd1367529811cbdcd62a1b4a019e945f
Aditya Shinde <shinde.aditya386@gmail.com>
2017-08-29 10:06:54 -0400

    Delete run_experiment.py

--- logistic_regression/run_experiment.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for i in range(1000,1100):
-    #print("For seed {}".format(i))
-    os.system("./logistic_regression.py ../../csci4360-fa17/assignments/assignment1/writeup/data/train.data ../../csci4360-fa17/assignments/assignment1/writeup/data/train.label ../../csci4360-fa17/assignments/assignment1/writeup/data/test_partial.data ../../csci4360-fa17/assignments/assignment1/writeup/data/test_partial.label "+str(i))
-    #print()
-
